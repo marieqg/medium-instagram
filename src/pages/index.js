dfd670aae6f004ff7d0acbde00e8e02bc5406a56
marieqg <mariequittelier@gmail.com>
2020-11-02 19:58:23 +1300

    front end

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import _get from "lodash/get";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query InstagramQuery {
+    allInstagramContent {
+      edges {
+        node {
+          localImage {
+            
+            childImageSharp {
+              fixed(width: 200, height: 200) {
+                ...GatsbyImageSharpFixed
+              }
+          }
+        }
+      }
+    }
+  }
+}
+`)
+  let arrayOfInstaImages = _get(data, 'allInstagramContent.edges')
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <div style={{ maxWidth: `900px`, marginBottom: `1.45rem`, display: 'flex' }}>
+        {arrayOfInstaImages.map((item, i) => {
+          return (
+            <div key={i} style={{ width: "200px", height: "200px" }}>
+              <Img fixed={item.node.localImage.childImageSharp.fixed} />
+            </div>)
+        })}
+      </div>
+
+    </Layout>)
+}
+
 
 export default IndexPage
